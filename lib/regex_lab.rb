f5bd171b6e01d67cdcb2135dfddc568f3393b597
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-28 12:03:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(//) == nil
+  if text.match(/^/) == nil
     return false 
   else 
     return true 
