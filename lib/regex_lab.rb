c56786644eb2d45b083f6711ccdde385a5834308
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-28 12:00:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,7 +6,6 @@ def starts_with_a_vowel?(word)
   end
 end
 
-
 def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/\bun\w+ing\b/)
 end
@@ -16,6 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  
 
 end
 
